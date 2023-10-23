9231f30363f143e7d6144dbb7eebfa838ae3faf5
Matheus Marinho <matheus.marinhodsp@gmail.com>
2023-10-23 15:55:48 -0300

    refac: removendo components de card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NavbarComponent } from 'src/app/components/navbar/navbar.component';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
-import { BigcardComponent } from './components/bigcard/bigcard.component';
 import { ContentComponent } from './pages/content/content.component';
 import { HomeComponent } from './pages/home/home.component';
 
@@ -14,7 +13,6 @@ import { HomeComponent } from './pages/home/home.component';
     HomeComponent,
     ContentComponent,
     NavbarComponent,
-    BigcardComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
